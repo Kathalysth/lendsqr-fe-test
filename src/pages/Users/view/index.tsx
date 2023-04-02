5a583bc8a0d0e84a56d2f93d3bc4fedf9b08e243
Chris Ehigimetor <bishopsirhchris@gmail.com>
2023-04-02 21:08:28 +0100

    feat: Add user view detail page

---
+++ src/pages/Users/view/index.tsx
@@ -0,0 +1,9 @@
+function UserView(): JSX.Element {
+  return (
+    <main id="app_content" className="app_content">
+      users view
+    </main>
+  )
+}
+
+export default UserView
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as SigninPage } from './Signin'
 export { default as DashboardPage } from './Dashboard'
 export { default as UsersPage } from './Users'
+export { default as UserViewPage } from './Users/view'
