65767d2b9df5ac74a137ff2ed4f46321743dc149
Chris Ehigimetor <bishopsirhchris@gmail.com>
2023-04-03 21:44:02 +0100

    feat: Add user view Page and tabs

---
+++ src/pages/Users/view/tabs/GeneralDetails.tsx
@@ -0,0 +1,123 @@
+function GeneralDetails(): JSX.Element {
+  return (
+    <div className="user_general_details">
+      <div className="user__general_details_group">
+        <h3 className="user__general_details_group_title">
+          Personal Information
+        </h3>
+        <div className="user__general_details_group_content col-5 ">
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Full Name</span>
+            <span className="user_detail_value">Grace Effiong</span>
+          </div>
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Phone Number</span>
+            <span className="user_detail_value">09098737583</span>
+          </div>
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Email Address</span>
+            <span className="user_detail_value">graceffiong@gmail.com</span>
+          </div>
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">BVN</span>
+            <span className="user_detail_value">4802939948384</span>
+          </div>
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Gender</span>
+            <span className="user_detail_value">Female</span>
+          </div>
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Marital Status</span>
+            <span className="user_detail_value">Single</span>
+          </div>
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Children</span>
+            <span className="user_detail_value">None</span>
+          </div>
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Type of residence</span>
+            <span className="user_detail_value">Parent&apos;s Apartment</span>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="user__general_details_group">
+        <h3 className="user__general_details_group_title">
+          Education and Employment
+        </h3>
+        <div className="user__general_details_group_content">
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Level of Education</span>
+            <span className="user_detail_value">B.Sc</span>
+          </div>
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Employment Status</span>
+            <span className="user_detail_value">Self Employed</span>
+          </div>
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Sector of Employment</span>
+            <span className="user_detail_value">FinTech</span>
+          </div>
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Duration of Employment</span>
+            <span className="user_detail_value">2 years</span>
+          </div>
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Office Email</span>
+            <span className="user_detail_value">graceffiong@gmail.com</span>
+          </div>
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Monthly income</span>
+            <span className="user_detail_value">₦200,000.00 - ₦400,000.00</span>
+          </div>
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Loan repayment</span>
+            <span className="user_detail_value">40,000</span>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="user__general_details_group">
+        <h3 className="user__general_details_group_title">Socials</h3>
+        <div className="user__general_details_group_content">
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Twitter</span>
+            <span className="user_detail_value">grace@lendsqr.com</span>
+          </div>
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Facebook</span>
+            <span className="user_detail_value">Grace Effiom</span>
+          </div>
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Instagram</span>
+            <span className="user_detail_value">@grace_effiom</span>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="user__general_details_group">
+        <h3 className="user__general_details_group_title">Guarantor</h3>
+        <div className="user__general_details_group_content">
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Full Name</span>
+            <span className="user_detail_value">Grace Effiong</span>
+          </div>
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Phone Number</span>
+            <span className="user_detail_value">09098737583</span>
+          </div>
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Email Address</span>
+            <span className="user_detail_value">graceffiong@gmail.com</span>
+          </div>
+          <div className="user__general_details_detail">
+            <span className="user_detail_label">Relationship</span>
+            <span className="user_detail_value">Sister</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default GeneralDetails
